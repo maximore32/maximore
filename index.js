68d6ada5646e9f70ad5fef4984d959701168c20f
maximore <moremaxi055@gmail.com>
2022-11-01 11:16:36 -0300

    no convierte a dolar

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ calculo();*/
 
 //------------------------SEGUNDA ENTREGA----------------------------------//
 
-class Moneda{
+/*class Moneda{
   constructor(nombre,cotizadolar,descripcion){
       this.nombre=nombre.toUpperCase();
       this.cotizadolar=parseInt(cotizadolar);
@@ -102,7 +102,126 @@ console.log(listamonedas);
 const devaluados=listamonedas.filter((ruins)=> ruins.cotizadolar >100).map((ruins)=>"Monedas devaluadas :"+ruins.nombre)
 console.log(devaluados);
 
-listamonedas[1].ConvertiraDolar();
+listamonedas[1].ConvertiraDolar();*/
+
+//---------------------------------------TERCERA ENTREGA-------------------------------------------//
+
+class Moneda{
+  constructor(id,nombre,cotizadolar,descripcion){
+      this.id=id;
+      this.nombre=nombre.toUpperCase();
+      this.cotizadolar=parseInt(cotizadolar);
+      this.descripcion=descripcion;     
+  }
+  
+
+}
+
+const listamonedas= [];
+listamonedas.push(new Moneda(1,"ars",300,"peso argentino"));
+listamonedas.push(new Moneda(2,"ves",3000,"peso venezolano"));
+listamonedas.push(new Moneda(3,"clp",0,01,"peso chileno"));
+listamonedas.push(new Moneda(4,"bob",0,14,"peso boliviano"));
+
+
+const carrito = [];
+let contenedor = document.getElementById("art");
+
+
+function mostrarMonedas(){
+    for(const moneda of listamonedas){
+        contenedor.innerHTML += `
+            
+                <div class="card-body">
+                    
+                    <h2 class="card-text">${moneda.nombre}</h2>
+                    <p class="card-text">${moneda.cotizadolar}</p>
+                    <p class="card-text">${moneda.descripcion}</p>
+                    <button id="btn${moneda.id}" class="btn btn-primary">Seleccionar</button>
+                </div>
+            
+        `;
+    }
+    
+    listamonedas.forEach(money => {
+          document.getElementById(`btn${money.id}`).addEventListener("click",function(){
+          agregarAlCarrito(money);
+        });
+    })
+       
+}
+
+mostrarMonedas()
+
+function agregarAlCarrito(monedaElegida){
+    carrito.push(monedaElegida);   
+    alert("Moneda Seleccionada: "+monedaElegida.nombre+" para convertir a U$$");
+    document.getElementById("monelegida").innerText = `
+       Moneda seleccionada:${monedaElegida.nombre}        
+    `;
+    const monedaAJson = JSON.stringify(monedaElegida);
+    localStorage.setItem("moneda",monedaAJson);
+    
+    
+       
+   
+}
+
+let contenedor2 = document.getElementById("art2");
+
+//-----------MONEDAS DEVAULADAS------------------------------------//
+
+function monedasDevaluadas(){
+  listamonedas.filter((ruins)=> ruins.cotizadolar >100).map((ruins)=>{
+    contenedor2.innerHTML+=`
+            
+    <div class="card-body">
+        
+        <h2 class="card-text">${ruins.nombre}</h2>
+        <p class="card-text">${ruins.cotizadolar}</p>
+        <p class="card-text">${ruins.descripcion}</p>        
+    </div>`;
+  })
+
+}
+monedasDevaluadas()
+
+let ultima = document.getElementById("last");
+
+//-----------ULTIMA MONEDA------------------------------------//
+
+function ultimaMoneda(){
+  let miMoneda=localStorage.getItem("moneda");
+  const jsonAObjeto = JSON.parse(miMoneda);
+  ultima.innerHTML=jsonAObjeto.nombre;
+}
+ultimaMoneda();
+
+//-----------FUNCION CONVERTIDORA------------------------------------//
+
+
+let local = document.getElementById("numselec").value;
+
+
+function ConvertiraDolar(){
+  
+  let miMoneda=localStorage.getItem("moneda");
+  const jsonAObjeto = JSON.parse(miMoneda);
+  let result = local / parseInt(jsonAObjeto.cotizadolar);
+
+  let p1 = document.getElementById("filtro");
+  let p2 = document.getElementById("resultado");
+  
+  
+
+  alert(result);
+  console.log(local);
+ 
+
+}
+
+
+
 
 
 
